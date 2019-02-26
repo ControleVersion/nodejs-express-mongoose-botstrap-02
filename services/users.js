ad3dfb6070aa2ca86f4728b7aaf2d4018c24272f
robertosm822 <robertomelo822@gmail.com>
2019-02-26 00:41:35 -0300

    Ultimos ajustes

--- services/users.js
+++ services/users.js
@@ -1,6 +1,13 @@
-exports.login = function(req, res){
-	res.render('login');
+var User = require('./../models/user');
+
+exports.login = function(req, res) {
+    res.render('login');
 };
-exports.register = function(req, res){
-	res.render('register');
+exports.register = function(req, res) {
+    User.create(req.body, function(err, user) {
+        if (err) {
+            return;
+        }
+        res.redirect('/users/login');
+    });
 };
